93c2f2809d7471fb04adabe97bbeac22d620c5d7
fajar-dev <fajarrivaldi2015@gmail.com>
2026-07-27 22:21:12 +0700

    feat: update brand logo and app icons

--- app/components/BrandLogo.vue
+++ app/components/BrandLogo.vue
@@ -1,6 +1,6 @@
 <template>
   <div class="flex items-center gap-3">
-    <div class="flex items-center justify-center w-9 h-9 rounded-lg bg-primary text-white shadow-sm shrink-0">
+    <!-- <div class="flex items-center justify-center w-9 h-9 rounded-lg bg-primary text-white shadow-sm shrink-0">
       <UIcon name="i-lucide-box" class="w-5 h-5 text-white" />
     </div>
     <div v-if="!isCollapsed" class="min-w-0">
@@ -10,7 +10,15 @@
       <p class="text-xs text-neutral-600">
         Asset Management System
       </p>
-    </div>
+    </div> -->
+    <NuxtLink to="/">
+      <div v-if="isCollapsed">
+        <img src="/logo.png" alt="Logo" class="w-10">
+      </div>
+      <div v-else>
+        <img src="/logo.png" alt="Logo">
+      </div>
+    </NuxtLink>
   </div>
 </template>
 
